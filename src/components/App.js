1c7294d69fa594c138ec8ba5dbf1a8b915043c4a
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-08 13:35:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  // let displayPara = false;
+  let [displayPara, setDisplayPara] = useState(false);
+  const buttonClicked = ()=>{
+    setDisplayPara(!displayPara);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button onClick={buttonClicked} id="click">Click</button>
+      {displayPara ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      : ""}
     </div>
   );
 }
